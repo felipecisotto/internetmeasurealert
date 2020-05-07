235ca4e677990153fb182bfbdbe258470374976a
Felipe Cisotto de Camargo <felipe.cisotto.camargo@everis.com>
2020-05-07 20:10:41 -0300

    Adicionando envio das métricas para o telegram

--- app/controller/CronController.js
+++ app/controller/CronController.js
@@ -1,9 +1,40 @@
 const cron = require("node-cron");
 const speedTestController = require("./SpeedTestController");
 const runSchedule = "0 0 */1 * * *";
-// const runSchedule = "0 */1 * * * *";
-
-cron.schedule(runSchedule, function() {
+var MeasureDAO = require("../DAO/SpeedTestDataDAO")
+var telegramController = require("../controller/TelegramController")
+const runMeasure = "0 0 */1 * * *";
+const sendResults = "0 30 23 */1 * * "
+cron.schedule(runMeasure, function() {
     speedTestController.runTest()
-  });
-  
+});
+
+cron.schedule(sendResults, function() {
+    
+    var measures = await MeasureDAO.getDayMeasures();
+    var message = new String();
+    var totalDownload = 0; 
+    var totalUpload = 0; 
+    var totalPing = 0 ;
+    var count = 0;
+
+    measures.array.forEach(element => {
+      
+      var download = Number(element.download/1000).toFixed(0)
+      var upload = Number(element.upload/1000).toFixed(0)
+      var ping = Number(element.ping).toFixed(0)
+      
+      totalDownload += download;
+      totalUpload += upload; 
+      totalPing += ping;
+      count++;
+
+      message += 'Hora: '+element.timestamp.getHours() +' -  Download: '+download +' Upload: '+upload + ' ping: '+ping+"\n"
+      
+    });
+
+    message = 'Média - Download: '+ totalDownload/count + ' Upload: '+totalUpload/count+' Ping: '+totalPing/count + "\n" +message
+
+    telegramController.sendMessage(message);
+
+});
